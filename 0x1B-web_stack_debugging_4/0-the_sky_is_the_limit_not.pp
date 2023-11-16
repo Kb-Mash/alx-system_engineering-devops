97d8dab50fce5136a11d2ae4c7ec1710042b1536
Kb-Mash <karabo.kmashile@gmail.com>
2023-11-16 11:13:04 -0800

    corrections

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,9 @@
 # increase the ULIMIT of the default file
-file_line { 'replace_line':
-  path => '/etc/default/nginx',
-  line => 'ULIMIT="-n 10000"',
-  match => '^ULIMIT',
+exec { 'replace_line':
+  command  => 'sed -i "s|ULIMIT=\"-n 15\"|ULIMIT=\"-n 10000\"|" /etc/default/nginx',
+  provider => 'shell',
 }
 -> exec { 'restart_nginx':
-  command => 'service nginx restart',
+  command  => 'service nginx restart',
   provider => 'shell',
 }
